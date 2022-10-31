30494b910d281b21ab4397fe7a946703b55cb820
Rolando Javier <rolandojavier.ramos@gmail.com>
2022-10-30 19:42:40 -0500

    chore: demo for soap web service

---
+++ src/main/java/com/howtodoinjava/xml/school/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2022.10.29 a las 09:22:54 PM COT 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.howtodoinjava.com/xml/school", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.howtodoinjava.xml.school;
